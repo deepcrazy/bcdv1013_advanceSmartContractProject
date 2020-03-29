2e0086dd16cdfb6f49a16369e3b5c8178081776d
deepcrazy <312.gupta@gmail.com>
2020-03-29 14:49:25 -0400

    modified deploy contract file for deploying FlashSale.sol contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,4 @@
 const FlashSale = artifacts.require("FlashSale");
-// const Wallet = artifacts.require("Wallet");
-// const WalletV2 = artifacts.require("WalletV2");
-
-// module.exports = function(deployer) {
-//   deployer.deploy(FlashSale);
-// };
 const registeredUsersMerkleRoot = "0xcbe9f791262c5be1860def4ba6ef53b7eb6c7adc365786e167a9a120de746de8";
 const existingUsersMerkleRoot = "0xf3891904928d0f51a25c774f1699f244505902ebd999b4bd797a6a5bf22327f2";
 
